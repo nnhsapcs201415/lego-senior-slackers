01f0749715650fca52912c1f6bdbacbcac49abc8
mtqiao <MTQiao@student.naperville203.org>
2017-05-09 15:08:58 -0500

    even more lul

---
+++ everything/MotorTutor1.java
@@ -0,0 +1,23 @@
+import lejos.nxt.Button;
+import lejos.nxt.LCD; 
+import lejos.nxt.Motor; 
+public class MotorTutor1
+{
+    public static void main(String[] args)
+    {
+        LCD.drawString("Program 1",0,0);
+        Button.waitForAnyPress();
+        LCD.clear();
+        Motor.B.forward();
+        Motor.C.forward();
+        LCD.drawString("FORWARD",0,0); 
+        Button.waitForAnyPress();
+        LCD.drawString("BACKWARD",0,0);
+        Motor.B.backward();
+        Motor.C.backward();
+        Button.waitForAnyPress();
+        Motor.B.stop();
+        Motor.C.stop(); 
+    }
+
+}
